501f7e5bd880c95412853ecf1eb831b909a4c40b
Mathew Hall <m@mhall.xyz>
2018-06-02 12:06:22 +0100

    Styling changes

--- src/components/header.js
+++ src/components/header.js
@@ -3,24 +3,32 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const Header = props => {
   return (
-    <View style={styles.headerView}>
-      <Text style={styles.headerText}>{props.title}</Text>
+    <View style={styles.View}>
+      <Text style={styles.Text}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    headerView: {
-        flex: 1,
-        maxHeight: 100,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        backgroundColor: "blue",
-    },
-    headerText: {
-        color: "white",
-    }
-})
+  View: {
+    flex: 1,
+    maxHeight: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#247BA0',
+    elevation: 20,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    position: 'relative',
+  },
+  Text: {
+    color: '#F3FFBD',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
 
 export default Header;
